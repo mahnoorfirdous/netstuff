996b2fac197c44477cc66a26872ad76e97ae4d08
mahnoor <mahnoor.ahmad@zortik.com>
2022-08-09 17:14:00 +0500

    added log handler

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	//TODO: Add routing implementations and imports
@@ -25,12 +26,21 @@ func homepage(rw http.ResponseWriter, req *http.Request) {
 func handle_requests() {
 	http.HandleFunc("/", homepage)
 	http.HandleFunc("/notebooks", getNotebooks)
+	http.HandleFunc("/log", logMessages)
 	log.Fatal(http.ListenAndServe(":8003", nil)) //TODO: Secure with TLS
 }
 
 func getNotebooks(rw http.ResponseWriter, req *http.Request) {
 	fmt.Println("Hit Endpoint /: notebooks")
 	json.NewEncoder(rw).Encode(notebooks)
+}
+
+func logMessages(rw http.ResponseWriter, req *http.Request) {
+	contents, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Printf("server: I don't get it %s\n", err)
+	}
+	log.Printf("Received %s\n", contents)
 }
 
 func main() {
