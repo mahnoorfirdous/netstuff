8990a2edfb2f78c339749397b6ba497b7865a585
mahnoor <mahnoor1243@gmail.com>
2022-09-07 01:10:27 +0500

    additions

--- reststuff/server/registration.go
+++ reststuff/server/registration.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterRequest struct {
@@ -18,6 +19,7 @@ type RegisterParams struct {
 	Name        string
 	AccountType string
 	Balance     float32
+	Password    string
 }
 
 // type UserResponse struct {
@@ -36,6 +38,7 @@ func (sv *Server) Register(ctx *gin.Context) {
 	err := ctx.ShouldBindJSON(request)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
 	argument := RegisterParams{
@@ -46,6 +49,20 @@ func (sv *Server) Register(ctx *gin.Context) {
 	}
 
 	sv.mockdb.Users[request.Email] = LocalStorage{Registration: argument}
-
+	sv.mockdb.storeHashedPassword(request.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 	ctx.JSON(http.StatusOK, gin.H{"response": argument})
+}
+
+func (as *AllStore) storeHashedPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 13)
+	return string(bytes), err
+}
+
+func (as *AllStore) verifyHashedPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
--- reststuff/server/user.go
+++ reststuff/server/user.go
@@ -1,5 +1,7 @@
 package restapi
 
+import "time"
+
 type LoginRequest struct {
 	Username string `json:"username" binding:"required,alphanum"`
 	Password string `json:"password" binding:"required,min=6"`
@@ -7,4 +9,11 @@ type LoginRequest struct {
 
 type LoginResponse struct {
 	AccessTok string `json:"accesstoken"`
+}
+
+type userResponse struct {
+	Name              string    `json:"name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
 }
